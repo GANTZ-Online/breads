35b3a15a8d9f846639689121402d4cb04869e110
Nicholas Mendoza <155332543+GANTZ-Online@users.noreply.github.com>
2024-05-06 19:01:14 -0700

    update

--- server.js
+++ server.js
@@ -28,3 +28,11 @@ app.get('/', (req, res) => {
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+
+// MIDDLEWARE
+app.use(express.static('public'))
+
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
