436892be23efad9c0b916402d47e382ec6c8b854
Nicholas Mendoza <155332543+GANTZ-Online@users.noreply.github.com>
2024-05-15 19:34:53 -0700

    update

--- views/show.jsx
+++ views/show.jsx
@@ -1,9 +1,9 @@
 const React = require('react');
-const Default = require('./layouts/default');
+const DefaultLayout = require('./layouts/default');
 
 function Show({ bread }) {
   return (
-    <Default>
+    <DefaultLayout>
       <h3>{bread.name}</h3>
       <p>
         and it {
@@ -13,13 +13,24 @@ function Show({ bread }) {
         } have gluten.
       </p>
       <img src={bread.image} alt={bread.name} />
-      <li><a href="/breads">Go home</a></li>
+      <p>Baked by {bread.baker}</p>
+      <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
+      <label htmlFor="baker">Baker</label>
+      <select name="baker" id="baker" defaultValue={bread.baker}>
+        <option value="Rachel">Rachel</option>
+        <option value="Monica">Monica</option>
+        <option value="Joey">Joey</option>
+        <option value="Chandler">Chandler</option>
+        <option value="Ross">Ross</option>
+        <option value="Phoebe">Phoebe</option>
+      </select>
       <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
         <input type='submit' value="DELETE" />
       </form>
-    </Default>
+    </DefaultLayout>
   );
 }
 
 module.exports = Show;
+
 
