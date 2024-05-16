ffeb3e2eac427caf16d39297227eeea2f44ea795
Nicholas Mendoza <155332543+GANTZ-Online@users.noreply.github.com>
2024-05-15 21:19:54 -0700

    BreadCRUD Custom Helper Methods (be-8-custom-helper-methods)

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -30,15 +30,17 @@ breads.get('/new', (req, res) => {
 })
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+      .then(foundBread => {
+        const bakedBy = foundBread.getBakedBy() 
+        console.log(bakedBy)
+        res.render('show', {
+            bread: foundBread
+        })
+      })
     })
-  } else {
-    res.send('404')
-  }
-})
+
 
 // CREATE
 breads.post('/', (req, res) => {
--- views/show.jsx
+++ views/show.jsx
@@ -13,7 +13,7 @@ function Show({ bread }) {
         } have gluten.
       </p>
       <img src={bread.image} alt={bread.name} />
-      <p>Baked by {bread.baker}</p>
+      <p>{bread.getBakedBy()}</p>
       <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
       <label htmlFor="baker">Baker</label>
       <select name="baker" id="baker" defaultValue={bread.baker}>
