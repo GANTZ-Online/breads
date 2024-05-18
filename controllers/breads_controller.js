950e27990eb65e72462062352f374466466631ef
Nicholas Mendoza <155332543+GANTZ-Online@users.noreply.github.com>
2024-05-18 11:47:36 -0700

    update

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,105 +1,115 @@
 const express = require('express');
 const breads = express.Router();
-const Baker = require('../models/baker.js')
+const Bread = require('../models/bread.js'); // Assuming Bread model is imported
+const Baker = require('../models/baker.js'); // Assuming Baker model is imported
 
-// in the new route
-breads.get('/new', (req, res) => {
+// NEW
+breads.get('/new', (req, res, next) => {
     Baker.find()
         .then(foundBakers => {
             res.render('new', {
                 bakers: foundBakers
-            })
-      })
-})
-
+            });
+        })
+        .catch(next); // Error handling middleware
+});
 
 // INDEX
-breads.get('/', (req, res) => {
-  Bread.find()
-      .then(foundBreads => {
-          res.render('index', {
-              breads: foundBreads,
-              title: 'Index Page'
-          })
-      })
-})
-
-// NEW
-breads.get('/new', (req, res) => {
-  res.render('new')
-})
-
+breads.get('/', (req, res, next) => {
+    Bread.find()
+        .then(foundBreads => {
+            res.render('index', {
+                breads: foundBreads,
+                title: 'Index Page'
+            });
+        })
+        .catch(next); // Error handling middleware
+});
 
 // SHOW
-breads.get('/:id', (req, res) => {
-  Bread.findById(req.params.id)
-      .populate('baker')
-      .then(foundBread => {
-        const bakedBy = foundBread.getBakedBy() 
-        console.log(bakedBy)
-        res.render('show', {
-            bread: foundBread
+breads.get('/:id', (req, res, next) => {
+    Bread.findById(req.params.id)
+        .populate('baker')
+        .then(foundBread => {
+            if (!foundBread) {
+                const error = new Error('Bread not found');
+                error.status = 404;
+                throw error;
+            }
+            const bakedBy = foundBread.getBakedBy(); // Assuming this method is defined in the Bread model
+            console.log(bakedBy);
+            res.render('show', {
+                bread: foundBread
+            });
         })
-      })
-    })
-
-
+        .catch(next); // Error handling middleware
+});
 
 // CREATE
-breads.post('/', (req, res) => {
-  if (!req.body.image) {
-    req.body.image = undefined
-  }
-  if(req.body.hasGluten === 'on') {
-    req.body.hasGluten = true
-  } else {
-    req.body.hasGluten = false
-  }
-  Bread.create(req.body)
-  res.redirect('/breads')
-})
+breads.post('/', (req, res, next) => {
+    if (!req.body.image) {
+        req.body.image = undefined;
+    }
+    req.body.hasGluten = req.body.hasGluten === 'on'; // Simplified conditional assignment
+    Bread.create(req.body)
+        .then(() => {
+            res.redirect('/breads');
+        })
+        .catch(next); // Error handling middleware
+});
 
-//UPDATE
-breads.put('/:id', (req, res) => {
-  if(req.body.hasGluten === 'on'){
-    req.body.hasGluten = true
-  } else {
-    req.body.hasGluten = false
-  }
-  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true }) 
-    .then(updatedBread => {
-      console.log(updatedBread) 
-      res.redirect(`/breads/${req.params.id}`) 
-    })
-})
+// UPDATE
+breads.put('/:id', (req, res, next) => {
+    req.body.hasGluten = req.body.hasGluten === 'on';
+    Bread.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then(updatedBread => {
+            if (!updatedBread) {
+                const error = new Error('Bread not found');
+                error.status = 404;
+                throw error;
+            }
+            console.log(updatedBread);
+            res.redirect(`/breads/${req.params.id}`);
+        })
+        .catch(next); // Error handling middleware
+});
 
 // EDIT
-breads.get('/:id/edit', (req, res) => {
-  Baker.find()
-    .then(foundBakers => {
-        Bread.findById(req.params.id)
-          .then(foundBread => {
-            res.render('edit', {
-                bread: foundBread, 
-                bakers: foundBakers 
-            })
-          })
-    })
-})
-
-
+breads.get('/:id/edit', (req, res, next) => {
+    Baker.find()
+        .then(foundBakers => {
+            Bread.findById(req.params.id)
+                .then(foundBread => {
+                    if (!foundBread) {
+                        const error = new Error('Bread not found');
+                        error.status = 404;
+                        throw error;
+                    }
+                    res.render('edit', {
+                        bread: foundBread,
+                        bakers: foundBakers
+                    });
+                });
+        })
+        .catch(next); // Error handling middleware
+});
 
 // DELETE
-breads.delete('/:id', (req, res) => {
-  Bread.findByIdAndDelete(req.params.id) 
-    .then(deletedBread => { 
-      res.status(303).redirect('/breads')
-    })
-})
-
-
+breads.delete('/:id', (req, res, next) => {
+    Bread.findByIdAndDelete(req.params.id)
+        .then(deletedBread => {
+            if (!deletedBread) {
+                const error = new Error('Bread not found');
+                error.status = 404;
+                throw error;
+            }
+            res.status(303).redirect('/breads');
+        })
+        .catch(next); // Error handling middleware
+});
 
 module.exports = breads;
+
 
 
   
